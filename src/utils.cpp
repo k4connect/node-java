b1db397950af32b35707005ed9aa587197560a22
Anurag Dulapalli <pdulapalli@gmail.com>
2020-08-24 16:39:32 -0400

    WIP Try now

--- src/utils.cpp
+++ src/utils.cpp
@@ -482,20 +482,24 @@ jvalueType javaGetArrayComponentType(JNIEnv *env, jobjectArray array) {
   return arrayComponentType;
 }
 
-#if (NODE_VERSION_AT_LEAST(4, 0, 0))
-v8::Local<v8::ArrayBuffer> newArrayBuffer(void* elems, size_t length) {
-  v8::Local<v8::ArrayBuffer> ab = v8::ArrayBuffer::New(v8::Isolate::GetCurrent(), length);
-  // FIXME: FOR DEBUGGING ONLY
-  printf("Gonna memcpy the ArrayBuffer...\n");
-  #if (NODE_VERSION_AT_LEAST(13, 0, 0))
+#if (NODE_VERSION_AT_LEAST(13, 0, 0))
+  v8::Local<v8::ArrayBuffer> newArrayBuffer(void* elems, size_t length) {
+    v8::Local<v8::ArrayBuffer> ab = v8::ArrayBuffer::New(v8::Isolate::GetCurrent(), length);
+    // FIXME: FOR DEBUGGING ONLY
+    printf("Gonna memcpy the ArrayBuffer...\n");
     printf("Standard (modern) V8 ArrayBuffer contents: %s\n", ab->GetBackingStore()->Data());
     memcpy(ab->GetBackingStore()->Data(), elems, length);
-  #else
+    return ab;
+  }
+#else
+  v8::Local<v8::ArrayBuffer> newArrayBuffer(void* elems, size_t length) {
+    v8::Local<v8::ArrayBuffer> ab = v8::ArrayBuffer::New(v8::Isolate::GetCurrent(), length);
+    // FIXME: FOR DEBUGGING ONLY
+    printf("Gonna memcpy the ArrayBuffer...\n");
     printf("Standard (legacy) V8 ArrayBuffer contents: %s\n", ab->GetContents()->Data());
     memcpy(ab->GetContents().Data(), elems, length);
-  #endif
-  return ab;
-}
+    return ab;
+  }
 #endif
 
 v8::Local<v8::String> javaCharToV8String(jchar c) {
